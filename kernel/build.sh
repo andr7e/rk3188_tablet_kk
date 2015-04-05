495dfa353bbcf9c92d7aec640f948a7b117dbd17
andr7e <andr13e@gmail.com>
2015-04-05 10:48:49 +0300

    build script

---
+++ kernel/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export PATH=$HOME/kernel_build/arm-eabi-4.6/bin/:$PATH
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=$HOME/kernel_build/arm-eabi-4.6/bin/arm-eabi-
+export LOCALVERSION=+
+
+make rk3188_ds1006h_v1_1_android-4.4_defconfig
+
+make -j2 Image
+
