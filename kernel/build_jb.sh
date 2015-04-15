0776729e7b57aea0c7bbf958e1cc1dc53bea20e4
andr7e <andr13e@gmail.com>
2015-04-15 21:24:41 +0300

    pmp5785 jb config

---
+++ kernel/build_jb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export PATH=$HOME/kernel_build/arm-eabi-4.6/bin/:$PATH
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=$HOME/kernel_build/arm-eabi-4.6/bin/arm-eabi-
+export LOCALVERSION=+
+
+make rk3188_pmp5785_defconfig
+
+make -j2 zImage
+
+./mkkrnlimg ./arch/arm/boot/zImage kernel.img
+
+zip -r out ./kernel.img
+mv ./out.zip ../kernel3.0.36+JB.zip
+
